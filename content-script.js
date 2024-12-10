84b869b987c9012f1fb52cd871c477d8bf3d0e65
Julien Allegre <jallegre@solem.fr>
2024-12-10 14:11:52 +0100

    Add gitlab support

--- content-script.js
+++ content-script.js
@@ -1,13 +1,49 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  const issueTitle = document.querySelector(
-    ".gh-header .gh-header-title .markdown-title"
-  ).innerText;
+  let titleSelector = document.querySelector(
+    ".detail-page-description h1.title"
+  );
+  if (!titleSelector) {
+    // try github
+    titleSelector = document.querySelector(
+      ".gh-header .gh-header-title .markdown-title"
+    );
+  }
+  const issueTitle = titleSelector?.innerText || "?";
 
-  const issueId = document.querySelector(
-    ".gh-header .gh-header-title span"
-  ).innerText;
+  let idSelector = document.querySelector(
+    'li[data-testid="breadcrumb-current-link"]'
+  );
+  if (!idSelector) {
+    // try github
+    idSelector = document.querySelector(
+      ".gh-header .gh-header-title span"
+    ).innerText;
+  }
 
-  const issueType = "bugfix";
+  const issueId = idSelector?.innerText || "?";
+
+  let typeFound = "?";
+  document
+    .querySelectorAll(".issuable-show-labels .gl-label-text-scoped")
+    .forEach((node) => {
+      var currentLabel = node.innerText.trim().toLowerCase();
+      switch (currentLabel) {
+        case "bug":
+          typeFound = "🐛 bugfix";
+          break;
+        case "feature":
+          typeFound = "✨ feature";
+          break;
+        default:
+          typeFound = currentLabel;
+      }
+
+      if (currentLabel === "bug" && currentLabel === "feature") {
+        typeFound = "bugfix";
+      }
+    });
+
+  const issueType = typeFound || "?";
 
   sendResponse({
     id: "grab",
